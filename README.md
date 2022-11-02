3d0173e0238f89e311ca3b49d85eb0adaa426bc3
Ivan <ivan.andriichuk.work@gmail.com>
2022-11-02 18:22:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+node v16.3.0
+
+1. npm install
+2. npm start
+
+ * admin login : vitaliypoltorak91@gmail.com
+ * admin password : Password1
+
+>back-end part for pet_spacex_gatsbyJs_strapi 
---
+++ src/admin/webpack.config.example.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack someVariables
+  // Important: return the modified someVariables
+  return config;
+};
